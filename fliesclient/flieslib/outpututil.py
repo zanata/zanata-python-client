17458bad208ea2f3de96df7226f2d83c781a998b
James Ni <kent.neo@gmail.com>
2011-03-02 13:08:16 +0800

    Change the class name of Logger

--- fliesclient/flieslib/outpututil.py
+++ fliesclient/flieslib/outpututil.py
@@ -19,8 +19,11 @@
 # License along with this program; if not, write to the
 # Free Software Foundation, Inc., 59 Temple Place, Suite 330,
 # Boston, MA  02111-1307  USA
+__all__ = (
+    "Logger",
+)
 
-class Logger():
+class Logger:
     def __init__(self):
         self.enable_infoprefix = False
         self.enable_errprefix = True
