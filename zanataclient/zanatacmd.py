924c8d622af62ba3f5196b59ac2b3c99b5197689
James Ni <kent.neo@gmail.com>
2012-02-15 16:14:00 +0800

    rhbz754869, fix the issue of pulling some files with a wrong path

--- zanataclient/zanatacmd.py
+++ zanataclient/zanatacmd.py
@@ -359,7 +359,7 @@ class ZanataCommand:
 
             if '/' in file_item: 
                 name = file_item.split('/')[-1]
-                folder = file_item.split(name)[0]
+                folder = file_item.rstrip(name)
                 request_name = file_item.replace('/', ',')
             else:
                 name = file_item
