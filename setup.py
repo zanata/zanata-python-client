e8fe8eb5593d1943a1a0df169ef8820b0e55c72f
Ding-Yi Chen <dingyichen@gmail.com>
2016-04-14 13:24:48 +1000

    fix(ZNTA-929) Use git-describe when in git environment, otherwise use VERSION-FILE

--- setup.py
+++ setup.py
@@ -13,37 +13,19 @@ def read(fname):
 
 
 def get_client_version():
-    number = ""
+    version_number = ""
+    # Use the version in VERSION-FILE
     path = os.path.dirname(os.path.realpath(__file__))
-    version_file = os.path.join(path, 'zanataclient/VERSION-FILE')
-    version_gen = os.path.join(path, 'VERSION-GEN')
-
-    p = subprocess.Popen(version_gen, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
-    output = p.stdout.readline()
-    number = output.rstrip()[len('version: '):]
-
-    subprocess.Popen("""
-    cat << EOF > MANIFEST.in
-    include zanataclient/VERSION-FILE
-    include CHANGELOG
-    include COPYING
-    include COPYING.LESSER
-    include zanata.ini
-EOF
-    """, shell=True)
-
-    if number == 'UNKNOWN':
-        try:
-            file = open(version_file, 'rb')
-            client_version = file.read()
-            file.close()
-            number = client_version[:-1][len('version: '):]
-        except IOError:
-            file = open(version_file, 'w')
-            file.write("version: UNKNOWN")
-            file.close
-
-    return number
+    version_file = os.path.join(path, 'zanataclient', 'VERSION-FILE')
+    try:
+        version = open(version_file, 'rb')
+        client_version = version.read()
+        version.close()
+        version_number = client_version.rstrip().strip('version: ')
+    except IOError:
+        print("Please run VERSION-GEN or 'make install' to generate VERSION-FILE")
+        version_number = "UNKNOWN"
+    return version_number
 
 requirements = read('requirements.txt').splitlines() + [
     'setuptools',
@@ -56,8 +38,8 @@ setup(
     include_package_data=True,
     install_requires=requirements,
     description="Zanata Python Client.",
-    author='Jian Ni, Ding-Yi Chen, Anish Patil',
-    author_email='jni@redhat.com, dchen@redhat.com, apatil@redhat.com',
+    author='Jian Ni, Ding-Yi Chen, Anish Patil, Sundeep Anand',
+    author_email='dchen@redhat.com, apatil@redhat.com, suandand@redhat.com',
     license='LGPLv2+',
     platforms=["Linux"],
     scripts=["zanata", "flies"],
